2ac7f2d8db233e6d02da9b726bf380eb25cbb53c
Angel Robles <Angelrobles05@gmail.com>
2025-04-22 18:05:58 -0400

    Structura del api

---
+++ Prueba.Tecnica.Web.Application/ResponseModels/ApiResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba.Tecnica.Web.Application.ResponseModels
+{
+    public class ApiResponse<T>
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public T Data { get; set; }
+        public List<string> Errors { get; set; }
+        public ApiResponse()
+        {
+            Success = true;
+            Message = string.Empty;
+            Data = default(T);
+            Errors = new List<string>();
+        }
+    }
+}
---
+++ Prueba.Tecnica.Web.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Prueba.Tecnica.Web.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
